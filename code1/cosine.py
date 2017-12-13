c6a32ff4fbb62293272fedad50781bd455859c5f
laughatwho <33539764+laughatwho@users.noreply.github.com>
2017-12-14 00:22:08 +0800

    Update cosine.py

--- code1/cosine.py
+++ code1/cosine.py
@@ -10,14 +10,11 @@ def cosine_sim(data, person1, person2):
     if len(commonmovie) == 0:
         return 0
 
-    # 求乘积和
     sum12 = sum(data[person1][movie]*data[person2][movie] for movie in commonmovie)
 
-    # 求平方和
     sum1sq = sum(pow(data[person1][movie], 2) for movie in data[person1])
     sum2sq = sum(pow(data[person2][movie], 2) for movie in data[person2])
 
-    # 计算分母
     den = sqrt(sum1sq) * sqrt(sum2sq)
     if den == 0:
         return 0
