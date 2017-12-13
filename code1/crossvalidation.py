02bfde2bbd0b73af6a24dd0af1560de93b3f65fb
laughatwho <33539764+laughatwho@users.noreply.github.com>
2017-12-14 00:23:20 +0800

    Update crossvalidation.py

--- code1/crossvalidation.py
+++ code1/crossvalidation.py
@@ -10,9 +10,7 @@ def divide_data(data, n_folds=2, leave_out_item=1):
     lists = dict_to_list(data)
     shuffle(lists)
 
-    # Get the number of interactions that each partition should have.
     partition_size = int(float(len(lists)/n_folds))
-    # To store the fold lists.
     list_folds = list()
     last = -1
     dic_fold = {}
@@ -33,14 +31,12 @@ def divide_data(data, n_folds=2, leave_out_item=1):
         dic_list_fold = {}
         dic_train_set = list_to_dict(train_set)
         dic_list_fold[fold] = list_to_dict(list_folds[fold])
-        # to leave n rate(s) out for each user
         if leave_out_item != 0:
             for key, value in dic_list_fold[fold].items():
                 try:
                     j = sample(range(len(value.values())), leave_out_item)
                     if leave_out_item > len(value.values()):
                         continue
-                    # j = randint(0, len(value.values()))
                     key1 = [key2 for key2 in value]
                     for i in j:
                         value[key1[i]] = 0
