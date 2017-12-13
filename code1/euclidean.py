4aa6d63c5e9a2e55cfd65c80da8a56101c38b61b
laughatwho <33539764+laughatwho@users.noreply.github.com>
2017-12-14 00:27:19 +0800

    Update euclidean.py

--- code1/euclidean.py
+++ code1/euclidean.py
@@ -2,20 +2,15 @@
 
 from math import sqrt
 
-'''
-    欧氏距离求相似度，距离越大，越相似
-'''
-
 def euclidean_sim(data, person1, person2):
 
     commonmovies = [movie for movie in data[person1] if movie in data[person2]]
+    
     if len(commonmovies) == 0:
         return 0
-# 平方和
-
+    
     sumSq = sum([pow(data[person1][movie] - data[person2][movie], 2) for movie in commonmovies])
-# 使最终结果是，越相似，距离越大。所以将上面距离取倒数即可
-
     sim = 1 / (1 + sqrt(sumSq))
+    
     return sim
 
